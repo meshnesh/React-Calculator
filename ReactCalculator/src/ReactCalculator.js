3bbb1e8550df08fc24a861f61a991aaab3e407a7
meshnesh <tonnie.nesh@gmail.com>
2017-01-17 19:52:42 +0300

    styled the display of the calculator app

--- ReactCalculator/src/ReactCalculator.js
+++ ReactCalculator/src/ReactCalculator.js
@@ -2,16 +2,21 @@
 
 import React, { Component } from 'react';
 import {
+    View,
     Text,
     AppRegistry
 } from 'react-native';
+import Style from './Style';
 
 class ReactCalculator extends Component {
 
     render() {
-        return (
-            <Text>Hello, World!</Text>
-        )
+         return (
+        <View style={Style.rootContainer}>
+             <View style={Style.displayContainer}></View>
+             <View style={Style.inputContainer}></View>
+        </View>
+    )
     }
 
 }
