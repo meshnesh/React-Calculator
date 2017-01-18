404281485baee7a9b37fcf42bb4c031ff71d3c2b
meshnesh <tonnie.nesh@gmail.com>
2017-01-18 18:23:25 +0300

    Added inputbutton.js for the buttos in the calculator

--- ReactCalculator/src/InputButton.js
+++ ReactCalculator/src/InputButton.js
@@ -3,7 +3,8 @@
 import React, { Component } from 'react';
 import {
     View,
-    Text
+    Text,
+  TouchableHighlight
 } from 'react-native';
 
 import Style from './Style';
@@ -12,9 +13,11 @@ export default class InputButton extends Component {
     
     render() {
         return (
-            <View style={Style.inputButton}>
+            <TouchableHighlight style={Style.inputButton}
+                                underlayColor="#193441"
+                                onPress={this.props.onPress}>
                 <Text style={Style.inputButtonText}>{this.props.value}</Text>
-            </View>
+            </TouchableHighlight>
         )
     }
     
